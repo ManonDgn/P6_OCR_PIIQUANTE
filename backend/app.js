786b97f54687efe3c27feb88cf76a6684df64a2b
Manon D <manon.digne@gmail.com>
2022-09-12 07:59:24 +0200

    Modif user

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 // Mongoose - MangoDB --
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://MiaDgn:AZERTY@cluster0.3ddovs0.mongodb.net/?retryWrites=true&w=majority',
